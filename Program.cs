1b250181d3cbcf077b88f9edd2a89af1d1a845d3
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-29 16:45:50 -0300

    Feat: Hub mapping

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ChatApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(options => 
+{
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new [] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
@@ -26,6 +32,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
